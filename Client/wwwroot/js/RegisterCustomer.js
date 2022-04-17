c1a6df5253ab0d124069f7db097a8f25f3475e35
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2022-04-17 08:34:25 +0700

    membuat page register

---
+++ Client/wwwroot/js/RegisterCustomer.js
@@ -0,0 +1,39 @@
+﻿$("#registerForm").submit(function (event) {
+    event.preventDefault();
+
+    const registerData = new Object();
+    registerData.First_Name = $("#registerForm #firstName").val()
+    registerData.Last_Name = $("#registerForm #lastName").val()
+    registerData.Email = $("#registerForm #email").val()
+    registerData.Password = $("#registerForm #password").val()
+    registerData.Phone_Number = $("#registerForm #phoneNumber").val()
+
+    console.log(registerData);
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "POST",
+        url: "https://localhost:44376/API/Customers/register",
+        dataType: "json",
+        data: JSON.stringify(registerData)
+    }).done(result => {
+        console.log(result)
+        Swal.fire({
+            icon: 'success',
+            title: 'Registrasi Sukses',
+            text: "Akun berhasil dibuat"
+        })
+    }).fail(error => {
+        console.log(error)
+
+        if (error.status == 400) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Registrasi Gagal',
+                text: error.responseJSON
+            })
+        }
+    })
+})
--- Helpdesk_Support_System/ViewModel/RegisterCustomerVM.cs
+++ Helpdesk_Support_System/ViewModel/RegisterCustomerVM.cs
@@ -7,10 +7,10 @@ namespace API.ViewModel
 {
     public class RegisterCustomerVM
     {
-        public string first_name { get; set; }
-        public string last_name { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string phone_number { get; set; }
+        public string Phone_Number { get; set; }
     }
 }
